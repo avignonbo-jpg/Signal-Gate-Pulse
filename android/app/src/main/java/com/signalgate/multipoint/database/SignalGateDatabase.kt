79875f8f028a77b62e2e36d660b34895d1d89d1b
avignonbo-jpg <avignonbo@gmail.com>
2026-07-27 16:54:30 -0700

    Add PendingCardEntity and enable schema export
    Added PendingCardEntity to the database and enabled schema export.

--- android/app/src/main/java/com/signalgate/multipoint/database/SignalGateDatabase.kt
+++ android/app/src/main/java/com/signalgate/multipoint/database/SignalGateDatabase.kt
@@ -41,9 +41,10 @@ import com.signalgate.multipoint.database.entities.UnifiedEntryEntity
         CallLogEntry::class,
         SettingEntry::class,
         SyncHistoryEntry::class,
+        PendingCardEntity::class,
     ],
     version = 2,
-    exportSchema = false
+    exportSchema = true
 )
 abstract class SignalGateDatabase : RoomDatabase() {
     abstract fun sourceDao(): SourceDao
@@ -51,6 +52,7 @@ abstract class SignalGateDatabase : RoomDatabase() {
     abstract fun callLogDao(): CallLogDao
     abstract fun settingDao(): SettingDao
     abstract fun syncHistoryDao(): SyncHistoryDao
+    abstract fun pendingCardDao(): PendingCardDao
 }
 
 /**
